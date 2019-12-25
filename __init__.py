f8082c0ec297ee40fc52ad8d3a40936b57f0527c
bluegreenmagick <bluegreenmagick@gmail.com>
2019-12-25 13:29:31 +0900

    reload note

--- __init__.py
+++ __init__.py
@@ -121,7 +121,7 @@ def myLinkHandler(reviewer, url, _old):
         orig_enc_val = base64.b64encode(orig_val.encode('utf-8')).decode('ascii')
         if enc_val == orig_enc_val: #enc_val may be the val of prev reviewed card.
             saveField(note, fld, new_val)
-            reviewer.card.q(reload=True)
+            reviewer.card.note(reload=True)
         else:
             tooltip("ERROR - Edit Field During Review Cloze\nSomething unexpected occured. The edit was not saved.%s"%fld)
             
