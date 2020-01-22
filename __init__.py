00cd741379d9c35e7fc70b269ef3d908f8050f8f
bluegreenmagick <bluegreenmagick@gmail.com>
2020-01-22 23:32:09 +0900

    process mime on paste

--- __init__.py
+++ __init__.py
@@ -9,15 +9,22 @@ Modified by <bluegreenmagick@gmail.com>
 
 from anki.hooks import addHook, wrap
 from anki.utils import htmlToTextLine
+
+from aqt import mw
 from aqt.editor import Editor
 from aqt.reviewer import Reviewer
-from aqt.utils import tooltip
-from aqt import mw
+from aqt.utils import tooltip, showInfo
+from aqt.editor import Editor
+from aqt.qt import QClipboard
 
 import base64
 import unicodedata
 import urllib.parse
+import json
 
+from .semieditor import semiEditorWebView
+
+editorwv = semiEditorWebView()
 
 bottom_js = """
 if(typeof EFDRConctrlkey != "function" && %(ctrl)s){
@@ -38,6 +45,180 @@ if(typeof EFDRConctrlkey != "function" && %(ctrl)s){
 }
 
 """
+
+#mostly from anki source qt/ts/src/editor.ts
+paste_js = """
+<script>
+  "use strict";
+window.parseHTML = function(str) {
+  let tmp = document.implementation.createHTMLDocument();
+  tmp.body.innerHTML = str;
+  return tmp.body.children;
+};
+window.pasteHTML = function (html, internal) {
+    html = filterHTML(html, internal, false);
+    document.execCommand("inserthtml", false, html);
+};
+window.filterHTML = function (html, internal, extendedMode) {
+    // wrap it in <top> as we aren't allowed to change top level elements
+    const top = parseHTML("<ankitop>" + html + "</ankitop>")[0];
+    if (internal) {
+        filterInternalNode(top);
+    }
+    else {
+        filterNode(top, extendedMode);
+    }
+    let outHtml = top.innerHTML;
+    if (!extendedMode) {
+        // collapse whitespace
+        outHtml = outHtml.replace(/[\\n\\t ]+/g, " ");
+    }
+    outHtml = outHtml.trim();
+    //console.log(`input html: ${html}`);
+    //console.log(`outpt html: ${outHtml}`);
+    return outHtml;
+};
+window.allowedTagsBasic = {};
+window.allowedTagsExtended = {};
+window.TAGS_WITHOUT_ATTRS = ["P", "DIV", "BR", "SUB", "SUP"];
+for (const tag of TAGS_WITHOUT_ATTRS) {
+    allowedTagsBasic[tag] = { attrs: [] };
+}
+TAGS_WITHOUT_ATTRS = [
+    "H1",
+    "H2",
+    "H3",
+    "LI",
+    "UL",
+    "OL",
+    "BLOCKQUOTE",
+    "CODE",
+    "PRE",
+    "TABLE",
+    "DD",
+    "DT",
+    "DL",
+    "B",
+    "U",
+    "I",
+    "RUBY",
+    "RT",
+    "RP",
+];
+for (const tag of TAGS_WITHOUT_ATTRS) {
+    allowedTagsExtended[tag] = { attrs: [] };
+}
+allowedTagsBasic["IMG"] = { attrs: ["SRC"] };
+allowedTagsExtended["A"] = { attrs: ["HREF"] };
+allowedTagsExtended["TR"] = { attrs: ["ROWSPAN"] };
+allowedTagsExtended["TD"] = { attrs: ["COLSPAN", "ROWSPAN"] };
+allowedTagsExtended["TH"] = { attrs: ["COLSPAN", "ROWSPAN"] };
+allowedTagsExtended["FONT"] = { attrs: ["COLOR"] };
+window.allowedStyling = {
+    color: true,
+    "background-color": true,
+    "font-weight": true,
+    "font-style": true,
+    "text-decoration-line": true,
+};
+window.filterExternalSpan = function (node) {
+    // filter out attributes
+    let toRemove = [];
+    for (const attr of node.attributes) {
+        const attrName = attr.name.toUpperCase();
+        if (attrName !== "STYLE") {
+            toRemove.push(attr);
+        }
+    }
+    for (const attributeToRemove of toRemove) {
+        node.removeAttributeNode(attributeToRemove);
+    }
+    // filter styling
+    toRemove = [];
+    for (const name of node.style) {
+        if (!allowedStyling.hasOwnProperty(name)) {
+            toRemove.push(name);
+        }
+        if (name === "background-color" && node.style[name] === "transparent") {
+            // google docs adds this unnecessarily
+            toRemove.push(name);
+        }
+    }
+    for (let name of toRemove) {
+        node.style.removeProperty(name);
+    }
+};
+allowedTagsExtended["SPAN"] = filterExternalSpan;
+// add basic tags to extended
+Object.assign(allowedTagsExtended, allowedTagsBasic);
+// filtering from another field
+window.filterInternalNode = function (node) {
+    if (node.style) {
+        node.style.removeProperty("background-color");
+        node.style.removeProperty("font-size");
+        node.style.removeProperty("font-family");
+    }
+    // recurse
+    for (const child of node.childNodes) {
+        filterInternalNode(child);
+    }
+};
+// filtering from external sources
+window.filterNode = function (node, extendedMode) {
+    // text node?
+    if (node.nodeType === 3) {
+        return;
+    }
+    // descend first, and take a copy of the child nodes as the loop will skip
+    // elements due to node modifications otherwise
+    const nodes = [];
+    for (const child of node.childNodes) {
+        nodes.push(child);
+    }
+    for (const child of nodes) {
+        filterNode(child, extendedMode);
+    }
+    if (node.tagName === "ANKITOP") {
+        return;
+    }
+    let tag;
+    if (extendedMode) {
+        tag = allowedTagsExtended[node.tagName];
+    }
+    else {
+        tag = allowedTagsBasic[node.tagName];
+    }
+    if (!tag) {
+        if (!node.innerHTML || node.tagName === "TITLE") {
+            node.parentNode.removeChild(node);
+        }
+        else {
+            node.outerHTML = node.innerHTML;
+        }
+    }
+    else {
+        if (typeof tag === "function") {
+            // filtering function provided
+            tag(node);
+        }
+        else {
+            // allowed, filter out attributes
+            const toRemove = [];
+            for (const attr of node.attributes) {
+                const attrName = attr.name.toUpperCase();
+                if (tag.attrs.indexOf(attrName) === -1) {
+                    toRemove.push(attr);
+                }
+            }
+            for (const attributeToRemove of toRemove) {
+                node.removeAttributeNode(attributeToRemove);
+            }
+        }
+    }
+};
+</script>
+"""
+
 
 #span, fld, ctrl is formatted. 
 #Capital EFDRC is just for easier code reading. Case doesn't matter.
@@ -71,6 +252,7 @@ if(typeof wrappedExceptForWhitespace != "function"){
             s.addRange(r);
         }
     }
+    
     window.EFDRCctrldown = function(){
         els = document.querySelectorAll("[data-EFDRC='true']"); 
         for(var e = 0; e < els.length; e++){
@@ -94,7 +276,30 @@ if(typeof wrappedExceptForWhitespace != "function"){
         }
     }
 
+    window.handlePaste = function(e){
+        var mimetype = ["text/html", "image/", "video/", "audio/"]
+        var paste = (e.clipboardData || window.clipboardData);
+        for(var x = 0; x < paste.types.length; x++){
+            mtype = paste.types[x];
+            to_send = false;
+            for(var y = 0; y < mimetype.length; y++){
+                if(mtype.indexOf(mimetype[y]) != -1){
+                    to_send = true;
+                    break;
+                }
+            }
+            if(to_send){
+                e.preventDefault();
+                pycmd("EFDRC#paste")
+                break;
+            }
+        }
+        
+    }
+
     window.EFDRCaddListeners = function(e, fld, spanBool){
+        e.addEventListener('paste', handlePaste);
+
         e.addEventListener('focus', function(event){
             pycmd("ankisave!focuson#" + fld);
             pycmd("ankisave!speedfocus#");
@@ -188,6 +393,7 @@ def edit(txt, extra, context, field, fullname):
     txt = """<%s data-EFDRCfield="%s" data-EFDRC="true">%s</%s>""" % (
         config['tag'], field, txt, config['tag'])
     txt += card_js % ({"fld":field, "span":span, "ctrl":ctrl})
+    txt += paste_js
     mw.reviewer.bottom.web.eval(bottom_js% ({"ctrl":ctrl}))
     return txt
 
@@ -272,6 +478,16 @@ def myLinkHandler(reviewer, url, _old):
         reviewer.web.eval("EFDRCctrldown()")
     elif url == "EFDRC#ctrlup":
         reviewer.web.eval("EFDRCctrlup()")
+    elif url == "EFDRC#paste":
+        #use processMime function a little modified in anki source!
+        mime = mw.app.clipboard().mimeData(mode=QClipboard.Clipboard)
+        html, internal = editorwv._processMime(mime)
+        html = editorwv.editor._pastePreFilter(html, internal)
+        tooltip(html)
+        reviewer.web.eval("pasteHTML(%s, %s);"% (json.dumps(html), json.dumps(internal)))
+    elif url.startswith("EFDRC#debug#"):
+        fld = url.replace("EFDRC#debug#", "")
+        showInfo(fld)
     else:
         return _old(reviewer, url)
 
---
+++ semieditor.py
@@ -0,0 +1,37 @@
+from aqt.editor import Editor, EditorWebView
+from aqt import mw
+
+
+class semiEditor():
+    #Necessary methods/variables for handling paste
+    _pastePreFilter = Editor._pastePreFilter
+    removeTags = Editor.removeTags
+    isURL = Editor.isURL
+    _retrieveURL = Editor._retrieveURL
+    inlinedImageToFilename = Editor.inlinedImageToFilename
+    _addPastedImage = Editor._addPastedImage
+    fnameToLink = Editor.fnameToLink
+    _addMediaFromData = Editor._addMediaFromData
+    urlToLink = Editor.urlToLink
+    urlToFile = Editor.urlToFile
+    inlinedImageToLink = Editor.inlinedImageToLink
+    
+    def __init__(self):
+        self.mw = mw
+
+
+        
+
+class semiEditorWebView():
+    #Necessary methods/variables for handling paste
+    _processMime = EditorWebView._processMime
+    _processUrls = EditorWebView._processUrls
+    _processText = EditorWebView._processText
+    _processImage = EditorWebView._processImage
+    _processHtml = EditorWebView._processHtml
+    
+    def __init__(self):
+        self.mw = mw
+        self.editor = semiEditor()
+        
+        
