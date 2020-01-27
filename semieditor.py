214f4af55f811da05da7a35d2b18867d0357ceef
bluegreenmagick <bluegreenmagick@gmail.com>
2020-01-27 18:24:59 +0900

    override progress window on paste with busy cursor Progress window steals focus from web and does not go along well with js focus and blur

--- semieditor.py
+++ semieditor.py
@@ -1,13 +1,15 @@
+from anki.hooks import wrap
+
+from aqt.progress import ProgressManager
 from aqt.editor import Editor, EditorWebView
 from aqt import mw
-
-
+from aqt.qt import QCursor, Qt
 
 class semiEditor(Editor):
     
     def __init__(self):
         self.mw = mw
-        self.parentWindow = mw
+        self.parentWindow = "EFDRCsemiedit"
 
 
         
@@ -17,5 +19,23 @@ class semiEditorWebView(EditorWebView):
     def __init__(self):
         self.mw = mw
         self.editor = semiEditor()
+
+
+def mystart(self, max=0, min=0, label=None, parent=None, immediate=False, _old = None):
+    if parent == "EFDRCsemiedit":
+        mw.EFDRCsemieditprogress = True
+        self.mw.app.setOverrideCursor(QCursor(Qt.WaitCursor))
+        return
+    else:
+        return _old(self, max=0, min=0, label=None, parent=None, immediate=False)
         
-        
+def myfinish(self, _old):
+    if mw.EFDRCsemieditprogress:
+        mw.EFDRCsemieditprogress = False
+        self.app.restoreOverrideCursor()
+        return
+    else:
+        return _old(self)
+
+ProgressManager.start = wrap(ProgressManager.start, mystart, "around")
+ProgressManager.finish = wrap(ProgressManager.finish, myfinish, "around")
