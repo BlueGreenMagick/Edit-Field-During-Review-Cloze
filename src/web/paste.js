7f53bbeed27026188d565bdbea5ea62f49e51783
bluegreenmagick <bluegreenmagick@gmail.com>
2021-02-20 12:06:58 +0900

    use editor.js file directly Should check editor.js path doesn't change in the future

--- src/web/paste.js
+++ src/web/paste.js
@@ -1,202 +1,43 @@
-// updated last at 2020 Feb 20
-// from qt.ts.src.editor.ts
 (function () {
   'use strict'
 
-  const parseHTML = function (str) {
-    const tmp = document.implementation.createHTMLDocument()
-    tmp.body.innerHTML = str
-    return tmp.body.children
-  }
-  const filterHTML = function (html, internal, extendedMode) {
-    // wrap it in <top> as we aren't allowed to change top level elements
-    const top = parseHTML('<ankitop>' + html + '</ankitop>')[0]
-    if (internal) {
-      filterInternalNode(top)
-    } else {
-      filterNode(top, extendedMode)
-    }
-    let outHtml = top.innerHTML
-    if (!extendedMode && !internal) {
-      // collapse whitespace
-      outHtml = outHtml.replace(/[\n\t ]+/g, ' ')
-    }
-    outHtml = outHtml.trim()
-    // console.log(`input html: ${html}`);
-    // console.log(`outpt html: ${outHtml}`);
-    return outHtml
-  }
-
-  const allowedTagsBasic = {}
-  const allowedTagsExtended = {}
-
-  let TAGS_WITHOUT_ATTRS = ['P', 'DIV', 'BR', 'SUB', 'SUP']
-  for (const tag of TAGS_WITHOUT_ATTRS) {
-    allowedTagsBasic[tag] = { attrs: [] }
-  }
-
-  TAGS_WITHOUT_ATTRS = [
-    'H1',
-    'H2',
-    'H3',
-    'I',
-    'LI',
-    'UL',
-    'OL',
-    'BLOCKQUOTE',
-    'CODE',
-    'PRE',
-    'RP',
-    'RT',
-    'RUBY',
-    'STRONG',
-    'TABLE',
-    'DD',
-    'DT',
-    'DL',
-    'B',
-    'U',
-    'I',
-    'RUBY',
-    'RT',
-    'RP',
-    'UL'
-  ]
-  for (const tag of TAGS_WITHOUT_ATTRS) {
-    allowedTagsExtended[tag] = { attrs: [] }
-  }
-
-  allowedTagsBasic.IMG = { attrs: ['SRC'] }
-
-  allowedTagsExtended.A = { attrs: ['HREF'] }
-  allowedTagsExtended.TR = { attrs: ['ROWSPAN'] }
-  allowedTagsExtended.TD = { attrs: ['COLSPAN', 'ROWSPAN'] }
-  allowedTagsExtended.TH = { attrs: ['COLSPAN', 'ROWSPAN'] }
-  allowedTagsExtended.FONT = { attrs: ['COLOR'] }
-
-  const allowedStyling = {
-    color: true,
-    'background-color': true,
-    'font-weight': true,
-    'font-style': true,
-    'text-decoration-line': true
-  }
-
-  const isNightMode = function () {
-    return document.body.classList.contains('nightMode')
-  }
-
-  const filterExternalSpan = function (node) {
-    // filter out attributes
-    let toRemove = []
-    for (const attr of node.attributes) {
-      const attrName = attr.name.toUpperCase()
-      if (attrName !== 'STYLE') {
-        toRemove.push(attr)
-      }
-    }
-    for (const attributeToRemove of toRemove) {
-      node.removeAttributeNode(attributeToRemove)
-    }
-    // filter styling
-    toRemove = []
-    for (const name of node.style) {
-      if (!Object.prototype.hasOwnProperty.call(allowedStyling, name)) {
-        toRemove.push(name)
-      }
-      if (name === 'background-color' && node.style[name] === 'transparent') {
-        // google docs adds this unnecessarily
-        toRemove.push(name)
-      }
-      if (isNightMode()) {
-        // ignore coloured text in night mode for now
-        if (name === 'background-color' || name === 'color') {
-          toRemove.push(name)
-        }
-      }
-    }
-    for (const name of toRemove) {
-      node.style.removeProperty(name)
-    }
-  }
-
-  allowedTagsExtended.SPAN = filterExternalSpan
-
-  // add basic tags to extended
-  Object.assign(allowedTagsExtended, allowedTagsBasic)
-
-  // filtering from another field
-  const filterInternalNode = function (node) {
-    if (node.style) {
-      node.style.removeProperty('background-color')
-      node.style.removeProperty('font-size')
-      node.style.removeProperty('font-family')
-    }
-    // recurse
-    for (const child of node.childNodes) {
-      filterInternalNode(child)
-    }
-  }
-
-  // filtering from external sources
-  const filterNode = function (node, extendedMode) {
-    // text node?
-    if (node.nodeType === 3) {
-      return
-    }
-
-    // descend first, and take a copy of the child nodes as the loop will skip
-    // elements due to node modifications otherwise
-
-    const nodes = []
-    for (const child of node.childNodes) {
-      nodes.push(child)
-    }
-    for (const child of nodes) {
-      filterNode(child, extendedMode)
-    }
-
-    if (node.tagName === 'ANKITOP') {
-      return
-    }
-
-    let tag
-    if (extendedMode) {
-      tag = allowedTagsExtended[node.tagName]
-    } else {
-      tag = allowedTagsBasic[node.tagName]
-    }
-    if (!tag) {
-      if (!node.innerHTML || node.tagName === 'TITLE') {
-        node.parentNode.removeChild(node)
-      } else {
-        node.outerHTML = node.innerHTML
-      }
-    } else {
-      if (typeof tag === 'function') {
-        // filtering function provided
-        tag(node)
-      } else {
-        // allowed, filter out attributes
-        const toRemove = []
-        for (const attr of node.attributes) {
-          const attrName = attr.name.toUpperCase()
-          if (tag.attrs.indexOf(attrName) === -1) {
-            toRemove.push(attr)
-          }
-        }
-        for (const attributeToRemove of toRemove) {
-          node.removeAttributeNode(attributeToRemove)
-        }
-      }
-    }
+  const appendScript = function (document, src, onLoad) {
+    const scriptEl = document.createElement('script')
+    scriptEl.setAttribute('src', src)
+    scriptEl.addEventListener('load', onLoad)
+    document.body.appendChild(scriptEl)
   }
 
   window.pasteHTML = function (html, internal) {
-    try {
-      html = filterHTML(html, internal, false)
-    } finally {
-      document.execCommand('inserthtml', false, html)
+    // import editor js in an invisible iframe
+    // to prevent reviewer window from being modified
+    const iframe = document.createElement('iframe')
+    iframe.style.display = 'none'
+    document.body.appendChild(iframe)
+    const iframeDoc = iframe.contentDocument
+    iframeDoc.body.innerHTML = '<div id="topbuts"></div><div id="fields"></div>'
+
+    // filterHTML is declared as a let, and is not attached to the window object.
+    // So we modify the script to attach it to the window object
+    const getEditorJsAndPaste = function () {
+      window.fetch('/_anki/js/editor.js')
+        .then(response => {
+          if (response.ok) return response.text()
+          else {
+            window.alert('ERROR: Edit Field During Review (Cloze) addon may not be compatible with this Anki version')
+          }
+        })
+        .then(text => {
+          const scriptEl = iframeDoc.createElement('script')
+          scriptEl.innerHTML = text + '\nwindow.filterHTML = filterHTML'
+          iframeDoc.body.appendChild(scriptEl)
+
+          // run after contents were loaded and run
+          const outHtml = iframe.contentWindow.filterHTML(html, internal, false)
+          document.execCommand('inserthtml', false, outHtml)
+        })
     }
+
+    appendScript(iframeDoc, '/_anki/js/vendor/jquery.js', getEditorJsAndPaste)
   }
 })()
